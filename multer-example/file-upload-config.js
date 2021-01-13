15d39983f1d2d68a8930761013c6fc63155c4e06
feychou <federica.recanatini@gmail.com>
2021-01-13 11:00:29 +0100

    Add mimetype check

--- multer-example/file-upload-config.js
+++ multer-example/file-upload-config.js
@@ -4,9 +4,17 @@ const multer  = require('multer');
 const publicFolder = path.join(__dirname, 'public');
 const uploadFolder = path.join(publicFolder, 'uploads');
 
+const isPicture = (name, mimetype) => {
+  return (
+    name.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/) &&
+    mimetype == "image/png" || mimetype == "image/jpg" || mimetype == "image/jpeg" || mimetype == "image/gif"
+  )
+}
+
 const fileFilter = (req, file, cb) => {
   // Accept image file types only
-  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+  const { originalname, mimetype } = file;
+  if (!isPicture(originalname, mimetype)) {
     req.fileValidationError = 'Only image files are allowed!';
     return cb('Only image files are allowed!', false);
   }
